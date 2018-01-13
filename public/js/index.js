2c0e25509e2e9269d5287f80a64b0d9738fd6196
Jorge Barbarian <jorgebarbarian@gmail.com>
2018-01-13 10:41:34 -0600

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+var socket = io();
+
     var quotes = ["Hoy sera tu dia","No te rindas","Eres fuerte"];
     var i = 0;
 
@@ -25,7 +27,18 @@ $(document).ready(function(){
     }, 6000);
     
 
+$(function () {
+    var socket = io();
+    $('form').submit(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
 
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+    });
+  });
 
 
 
